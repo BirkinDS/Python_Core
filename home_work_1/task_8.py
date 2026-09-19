5bfc28da1537ecf4570d81eb33872c4c1c7a6848
BirkinDS <myclefable@gmail.com>
2026-09-19 11:45:52 +0300

    Add task 6-10.

---
+++ home_work_1/task_8.py
@@ -0,0 +1,11 @@
+# task description
+# Вывести, входит ли строка1 в строку2 | Пример: employ и employment
+
+# solution
+Anacondaz = "Ангел"
+NoizeMC = "Ангел_трек"
+if Anacondaz in NoizeMC:
+    print("Входит")
+else:
+    print("Не входит")
+
