f5ddb864e3a2c26f88831b5bdbcc7b3a8801cd67
BirkinDS <myclefable@gmail.com>
2026-09-25 13:13:49 +0300

    Вернул 10 заданий.

---
+++ homework_1/task_2.py
@@ -0,0 +1,7 @@
+# task description
+# Заменить символ "#" на символ "/" в строке: www.my_site.com#about
+
+# solution
+line = "www.my_site.com#about"
+line = line.replace("#", "/")
+print(line)
---
+++ homework_1/task_8.py
@@ -0,0 +1,10 @@
+# task description
+# Вывести, входит ли строка1 в строку2 | Пример: employ и employment
+
+# solution
+Anacondaz = "Ангел"
+NoizeMC = "Ангел_трек"
+if Anacondaz in NoizeMC:
+    print("Входит")
+else:
+    print("Не входит")
